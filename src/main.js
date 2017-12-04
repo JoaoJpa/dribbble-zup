8d04b1d03f2c10747aebbb6681ee964e928cc9ed
joaoamaral <joaoamaral@softbox.com.br>
2017-12-04 19:31:33 -0200

    ajuste na cor do tema :)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import('../node_modules/vuetify/dist/vuetify.min.css')
+import ('../node_modules/vuetify/dist/vuetify.min.css')
 import Vue from 'vue'
 import App from './app/App.vue'
 import router from './router'
@@ -10,12 +10,14 @@ import axios from 'axios'
 
 Vue.use(VueAxios, axios)
 
-Vue.use(Vuetify, {theme:{
-    primary: '#2f2f2ffa',
+Vue.use(Vuetify, {
+  theme: {
+    primary: 'rgba(47,47,47,0.98)',
     secondary: '#999',
     accent: '#8c9eff',
     error: '#b71c1c'
-}});
+  }
+});
 
 Vue.config.productionTip = false;
 
@@ -24,5 +26,7 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
